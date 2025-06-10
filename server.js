a2c82c926a50a1cc5548133b6cd8de9a2522b90e
Ajilps <144360641+Ajilps@users.noreply.github.com>
2025-06-10 16:52:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 });
 
 // --- Database Model (with Validation) ---
-const Item = sequelize.define('Student', {
+const Item = sequelize.define('Students', {
   name: {
     type: DataTypes.STRING,
     allowNull: false,
